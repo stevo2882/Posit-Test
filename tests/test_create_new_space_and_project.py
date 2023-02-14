0b9fad8cad3f0958da05e0d8c8c4feb7fa6c6197
Steven Mays <stevenmays@Abbies-MacBook-Air.local>
2023-02-13 23:28:50 -0500

    uncomment new space creation

--- tests/test_create_new_space_and_project.py
+++ tests/test_create_new_space_and_project.py
@@ -5,11 +5,11 @@ def test_create_new_space_and_project(driver, login):
     """Verify that a user can create a new space and project in Posit Cloud and the IDE is loaded."""
     # Create a new space and verify it is displayed in the space menu
     base_page = BasePage(driver)
-    # new_space_modal = base_page.create_new_space()
-    # new_space_modal.enter_space_name("test_new_space")
-    # new_space_modal.click_create_button()
+    new_space_modal = base_page.create_new_space()
+    new_space_modal.enter_space_name("test_new_space")
+    new_space_modal.click_create_button()
     spaces = base_page.get_spaces()
-    assert "Your Workspace" in spaces
+    assert "test_new_space" in spaces
 
     # Create a new project and verify that the Posit Cloud IDE loads
     ide_page = base_page.create_new_rstudio_project()
